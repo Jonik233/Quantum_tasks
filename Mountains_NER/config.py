6a0f05e3eaeaca63a253c7b87ad1c0bb1f587a89
Vlad Mashtalier <master@Vlads-MacBook-Air.local>
2026-07-15 19:18:32 +0300

    label configuration added

---
+++ Mountains_NER/config.py
@@ -0,0 +1,13 @@
+label2id = {
+    "O": 0,
+    "B-MOUNTAIN": 1,
+    "I-MOUNTAIN": 2
+}
+
+id2label = {
+    0: "O",
+    1: "B-MOUNTAIN",
+    2: "I-MOUNTAIN"
+}
+
+label_names = ["O", "B-MOUNTAIN", "I-MOUNTAIN"]
