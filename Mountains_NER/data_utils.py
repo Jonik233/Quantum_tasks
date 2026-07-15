b7333fb91bbf45df3dbde0d0b6830fbf30a2da09
Vlad Mashtalier <master@Vlads-MacBook-Air.local>
2026-07-16 00:35:53 +0300

    module restructured to load data from s3

--- Mountains_NER/data_utils.py
+++ Mountains_NER/data_utils.py
@@ -1,34 +1,29 @@
 import os
 import ast
 import pandas as pd
-from dotenv import load_dotenv
 from dataset import MountainsDataset
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from config import TRAIN_PARAMETERS, VAL_PARAMETERS
 from transformers import DistilBertTokenizerFast, DataCollatorForTokenClassification
 
-load_dotenv()
 
+def load_dataloaders(tokenizer:DistilBertTokenizerFast,
+                     train_parameters:dict,
+                     val_parameters:dict,
+                     train_data_path:str,
+                     val_data_path:str):
 
-def load_datasets(tokenizer:DistilBertTokenizerFast):
-    df = pd.read_csv(os.environ["TRAIN_DATA_PATH"], converters={"marker": ast.literal_eval})
+    train_file = os.path.join(train_data_path, "train_dataset.csv")
+    val_file = os.path.join(val_data_path, "val_dataset.csv")
 
-    ### Sampling train and validation data ###
-    train_data, val_data = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
-    train_data = train_data.reset_index(drop=True)
-    val_data = val_data.reset_index(drop=True)
+    train_df = pd.read_csv(train_file, converters={"marker": ast.literal_eval})
+    val_df = pd.read_csv(val_file, converters={"marker": ast.literal_eval})
 
-    train_dataset = MountainsDataset(train_data, tokenizer)
-    val_dataset = MountainsDataset(val_data, tokenizer)
+    train_dataset = MountainsDataset(train_df, tokenizer)
+    val_dataset = MountainsDataset(val_df, tokenizer)
 
-    return train_dataset, val_dataset
-
-
-def load_dataloaders(tokenizer:DistilBertTokenizerFast):
-    train_dataset, val_dataset = load_datasets(tokenizer)
     collate_fn = DataCollatorForTokenClassification(tokenizer=tokenizer)
-    train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, **TRAIN_PARAMETERS)
-    val_dataloader = DataLoader(val_dataset, collate_fn=collate_fn, **VAL_PARAMETERS)
+
+    train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, **train_parameters)
+    val_dataloader = DataLoader(val_dataset, collate_fn=collate_fn, **val_parameters)
 
     return train_dataloader, val_dataloader
