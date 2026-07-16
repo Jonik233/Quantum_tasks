c8743be2548ae27e3c08a2fe678fc12dc45c2292
Vlad Mashtalier <master@Vlads-MacBook-Air.local>
2026-07-16 15:55:41 +0300

    module for triggering a sagemaker training job

---
+++ Mountains_NER/src/main.py
@@ -0,0 +1,32 @@
+import sagemaker
+from sagemaker.huggingface import HuggingFace
+
+sagemaker_session = sagemaker.Session()
+role = "arn:aws:iam::519763207317:role/NERunner"
+
+
+huggingface_estimator = HuggingFace(
+    entry_point="train.py",
+    source_dir="./src",                 # Directory containing train.py, model.py, config.py, etc.
+    role=role,
+    instance_count=1,
+    instance_type="ml.g6.2xlarge",
+    transformers_version="4.36",
+    pytorch_version="2.1",
+    py_version="py310",
+    output_path="s3://qunatum-tasks-bucket/Mountains_NER/output/",
+    hyperparameters={
+        "epochs": 3,
+        "train_batch_size": 16,
+        "valid_batch_size": 16,
+        "learning_rate": 2e-5
+    },
+    enable_sagemaker_metrics=True
+)
+
+# Trigger the train job
+# SageMaker will map the "train" key to the SM_CHANNEL_TRAIN env variable inside train.py
+huggingface_estimator.fit({
+    "train": "s3://qunatum-tasks-bucket/Mountains_NER/data/",
+    "valid": "s3://qunatum-tasks-bucket/Mountains_NER/data/"
+})
