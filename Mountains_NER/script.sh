fce56f621c1dfac632d022979ac828046eec5187
Vlad Mashtalier <master@Vlads-MacBook-Air.local>
2026-07-19 16:19:04 +0300

    script for downloading the model from S3 bucket

---
+++ Mountains_NER/script.sh
@@ -0,0 +1,7 @@
+### Download the compressed model from S3 bucket ###
+
+aws s3 cp s3://qunatum-tasks-bucket/Mountains_NER/output/huggingface-pytorch-training-2026-07-18-12-30-59-495/output/model.tar.gz .
+
+mkdir model_artifacts
+
+tar -xvf model.tar.gz -C model_artifacts
